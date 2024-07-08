e7fab675291705d2884d8e82c5c50573fa90249e
Alexis Tapia <tapiaalexv@gmail.com>
2024-07-08 12:18:19 -0500

    Operadores binarios

--- Operadores/Operadores/Program.cs
+++ Operadores/Operadores/Program.cs
@@ -10,6 +10,31 @@ namespace Operadores
     {
         static void Main(string[] args)
         {
+            /*Operadores binarios*/
+
+            //Suma
+            Console.WriteLine(5 + 6);
+            int num1 = 3;
+            double num2 = 12.5;
+            Console.WriteLine(num1 + num2);
+            
+            //Concatenacion de cadenas
+            string saludo = "Hola";
+            string nombre = "Alexis";
+            Console.WriteLine(saludo + " " + nombre);
+
+            //Resta
+            Console.WriteLine(5 - 6);
+            int num3 = 3;
+            double num4 = 12.5;
+            Console.WriteLine(num1 - num2);
+
+            //Multiplicacion
+            Console.WriteLine(5 * 6);
+            int num5 = 3;
+            double num6 = 12.5;
+            Console.WriteLine(num1 * num2);
+
         }
     }
 }

9dc75fed28626b094e23e10e7161fe87f3c9e295
Alexis Tapia <tapiaalexv@gmail.com>
2024-07-08 14:14:25 -0500

    Operadores Igualdad Comparacion

---
+++ OperadoresIgualdadComparacion/OperadoresIgualdadComparacion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresIgualdadComparacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Operadores de Igualdad*/
+            // ==, !=
+            int a = 1, b = 2;
+
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+
+            /*Operadores de Comparacion*/
+            // <, >, <=, >=
+            Console.WriteLine(a < b);
+            Console.WriteLine(a > b);
+            Console.WriteLine(a <= b);
+            Console.WriteLine(a >= b);
+
+
+        }
+    }
+}

bf5716593dee1c64808c0721e47e9ecb027cb7b6
Alexis Tapia <tapiaalexv@gmail.com>
2024-07-08 12:23:36 -0500

    Fin de operadores binarios

--- Operadores/Operadores/Program.cs
+++ Operadores/Operadores/Program.cs
@@ -17,7 +17,7 @@ namespace Operadores
             int num1 = 3;
             double num2 = 12.5;
             Console.WriteLine(num1 + num2);
-            
+
             //Concatenacion de cadenas
             string saludo = "Hola";
             string nombre = "Alexis";
@@ -34,6 +34,17 @@ namespace Operadores
             int num5 = 3;
             double num6 = 12.5;
             Console.WriteLine(num1 * num2);
+
+            //Division
+            Console.WriteLine(13 / 3); //Si se usan entereos el resultado sera entero
+            Console.WriteLine(13 / 3.0); //Si se quiere un decimal basta con agregar un .float a cualquiera
+
+            //Resto
+            Console.WriteLine(5 % 2);
+
+            //Jerarquia de Operadores Binarios
+            //1. * / %
+            //2. + -
 
         }
     }

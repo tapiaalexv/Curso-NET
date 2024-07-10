c33a7e73f722405c22539daf359d5f9daea6cb76
Alexis Tapia <tapiaalexv@gmail.com>
2024-07-10 12:22:54 -0500

    Multiples Valores con Return

--- TipoDatosExtras/TipoDatosExtras/Program.cs
+++ TipoDatosExtras/TipoDatosExtras/Program.cs
@@ -16,7 +16,7 @@ namespace TipoDatosExtras
             //Es necesario dar un valor antes de pasar por referencia
             int numAR = 0;
 
-            Console.WriteLine("El valor ANTES de la refenrecia {0}",numAR);
+            Console.WriteLine("El valor ANTES de la refenrecia {0}", numAR);
 
             PruebaRef(ref numAR);
             Console.WriteLine("El valor DESPUES de la refenrecia {0}", numAR);
@@ -33,10 +33,39 @@ namespace TipoDatosExtras
             PruebaOut(out num2AR);
             Console.WriteLine("El valor DESPUES de OUT {0}", num2AR);
 
-            /**/
+            /*VAR*/
+            //Se debe inicializar en la misma linea
+            var nombre = "Luis";
+            var cedula = 1726503103;
+            var sueldo = 900.50;
+
+            /*TUPLA*/
+            //Siempre debe ser 2 o + elementos
+            //(tipo1, tipo2, ...) nombreVariable = (valor1, valor2, ...);
+
+            (int, int, int) numPersonas = (1, 2, 3);
+            (int, double, string, char) numPersonas2 = (27, 10.0, "Clarissa", 'F');
+
+            Console.WriteLine(numPersonas.Item2 + " - " + numPersonas2.Item2 + " - " + numPersonas2.Item3);
+
+            //Cuando se tiene diferentes tipos en los valores usaremos VAR
+            //[var] nombreVariable = (valor1, valor2, ...); 
+            var persona1 = ("Alexis", 'M', 2974368, 26);
+            Console.WriteLine(persona1.Item2 + " - " + persona1.Item1 + " - " + persona1.Item3);
+
+            //Podemos nombrar los valores
+            //Para tipo implicito
+            var persona2 = (nombre: "Alexis", sexo: 'M', telefono: 2974368, edad: 26);
+            Console.WriteLine(persona2.nombre + " - " + persona2.sexo + " - " + persona2.edad + " - " + persona2.telefono);
+
+            //Para tipo explicito
+            (string nombre, char sexo, int telefono, byte edad) persona3 = ("Alexis", 'M', 2974368, 26);
+            Console.WriteLine(persona3.nombre + " - " + persona3.sexo + " - " + persona3.edad + " - " + persona3.telefono);
+
         }
 
-        static void PruebaRef(ref int numPa){ 
+        static void PruebaRef(ref int numPa)
+        {
             //Modificamos a traves de referencia el valor
             numPa = 10;
         }
